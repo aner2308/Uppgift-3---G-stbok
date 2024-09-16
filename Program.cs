702aa080eb088c131543569e5e0f68aba361d53a
Anton <erikssonanton94@gmail.com>
2024-09-16 04:15:06 +0200

    test av menysystem. Har just nu ett problem med att de översta två raderna i menyn upprepas vid varje loop

--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace GuestbookApp
+while (true)
 {
-    class Program 
+    //Rensar konsollen innan menyn skrivs ut
+    Console.Clear();
+
+    //Skriver ut menyn med menyval
+    Console.WriteLine("Välkommen till Antons gästbok!");
+    Console.WriteLine("  1. Skapa inlägg");
+    Console.WriteLine("  2. Ta bort inlägg");
+    Console.WriteLine("  3. Visa alla inlägg");
+    Console.WriteLine("  4. Avsluta");
+    Console.Write("Välj ett alternativ: ");
+
+    //Låter användaren skriva in sitt menyval
+    string choice = Console.ReadLine();
+
+    //Jämför användarens inmantning med en switch-sats
+    switch (choice)
     {
-        static void Main() 
-        {
-            Console.WriteLine("Hej världen!");
-        }
+        case "1":
+            Console.WriteLine("Menyval 1 fungerar!");
+            break;
+
+        case "2":
+            Console.WriteLine("Menyval 2 fungerar!");
+            break;
+
+        case "3":
+            Console.WriteLine("Menyval 3 fungerar!");
+            break;
+
+        case "4":
+            Console.WriteLine("Menyval 4 fungerar!");
+            return;
+
+        default:
+            Console.WriteLine("Error: Felaktig inmatning. Försök igen...");
+            break;
     }
+
+    Console.WriteLine("Tryck på valfri tangent för att fortsätta...");
+    Console.ReadKey();
 }
